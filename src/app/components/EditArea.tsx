4f99789ac167269e464c81dcbeb45efc070d072d
gitowa12 <118157881+gitowa12@users.noreply.github.com>
2024-03-20 23:08:49 +0900

    feat: アイコンの削除機能を追加

--- src/app/components/EditArea.tsx
+++ src/app/components/EditArea.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import TextArea from "./TextArea";
 import { v4 as uuidv4 } from "uuid";
 
@@ -11,14 +11,16 @@ type ImageInfo = {
 
 const EditArea: React.FC = () => {
   const [images, setImages] = useState<ImageInfo[]>([]);
+  const [isDragging, setIsDragging] = useState(false); // ドラッグ状態を追跡
+  const deleteBoxRef = useRef<HTMLDivElement | null>(null);
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
 
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
     const imageSrc = e.dataTransfer.getData("imageSrc");
+    console.log(e);
     if (imageSrc) {
       const newImage = {
         id: uuidv4(),
@@ -32,18 +34,26 @@ const EditArea: React.FC = () => {
   };
 
   const dragEnd = (e: React.DragEvent<HTMLImageElement>) => {
-    console.log(e.target);
+    // console.log(e.target);
+    //アイコンのidと移動後の位置情報を取得 ※位置情報は移動前位置からの相対位置
     const id = e.target.id;
     const movedX = e.nativeEvent.offsetX;
     const movedY = e.nativeEvent.offsetY;
     updateImagePosition(id, movedX, movedY);
+
+    //ドラッグが終わればゴミ箱を非表示
+    setIsDragging(false);
+
+    // const deleteBox = deleteBoxRef.current;
+    // deleteBox.className = "transition bg-red-400 rounded-lg w-16 h-16 fixed top-[150px] left-1/2 ";
   };
 
   const updateImagePosition = (id: string, addX: number, addY: number) => {
+    //idでstateから検索して、位置情報を更新してセットする
     setImages(
       images.map((image) => {
         if (image.id === id) {
-          console.log(image);
+          // console.log(image);
           const newX = image.x + addX;
           const newY = image.y + addY;
 
@@ -55,7 +65,21 @@ const EditArea: React.FC = () => {
     );
   };
 
-  // 例: IDが2の画像の位置を更新
+  //削除用にIDをdataTransferにセット
+  const handleDragStart = (e: React.DragEvent<HTMLImageElement>) => {
+    const id = e.target.id;
+    e.dataTransfer.setData("id", id);
+    //ドラッグ中はゴミ箱を表示
+    setIsDragging(true);
+  };
+
+  const handleDeleteDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    const id = e.dataTransfer.getData("id");
+    console.log(id);
+    setImages(images.filter((image) => image.id !== id));
+    //ゴミ箱に入ったら非表示
+    setIsDragging(false);
+  };
 
   return (
     <div>
@@ -63,13 +87,14 @@ const EditArea: React.FC = () => {
         id="editor"
         className="mt-4 h-fit relative"
         onDragOver={handleDragOver}
-        onDrop={handleDrop}
+        onDrop={(e) => handleDrop(e)}
       >
         {images.map((image, index) => (
           <img
             key={index}
             id={image.id}
             draggable="true"
+            onDragStart={(e) => handleDragStart(e)}
             onDragEnd={(e) => dragEnd(e)}
             src={image.src}
             className="w-3"
@@ -81,7 +106,17 @@ const EditArea: React.FC = () => {
             alt=""
           />
         ))}
-        <TextArea />
+        <TextArea></TextArea>
+      </div>
+      <div
+        ref={deleteBoxRef}
+        className={`transition-all duration-300 ease-in-out bg-red-400 rounded-xl p-2 h-10 fixed left-1/2 ${
+          isDragging ? "top-[50px]" : "-top-[50px]" // ドラッグ中のみ表示
+        }`}
+        onDragOver={(e) => e.preventDefault()}
+        onDrop={(e) => handleDeleteDrop(e)}
+      >
+        ゴミ箱
       </div>
     </div>
   );
--- src/app/components/IconList.tsx
+++ src/app/components/IconList.tsx
@@ -4,13 +4,31 @@ const IconList = () => {
   const handleDragStart = (e: React.DragEvent<HTMLDivElement>) => {
     const imageSrc = e.target.src; // ドラッグされた画像のsrcを取得
     e.dataTransfer.setData("imageSrc", imageSrc); // 転送データに画像のsrcをセット
-    const imagesrc = e.dataTransfer.getData(imageSrc);
+    // const imagesrc = e.dataTransfer.getData(imageSrc);
     // console.log(imageSrc);
   };
 
   return (
     <div className="mt-4 p-1 border border-blue-500 flex gap-5 fixed">
-      <div id="icons" onDragStart={(e) => handleDragStart(e)}>
+      <div id="icons" className="flex w-fit" onDragStart={(e) => handleDragStart(e)}>
+        <img
+          className="w-8"
+          src="http://localhost:3000/breath.png"
+          draggable="true"
+          alt="breath"
+        ></img>
+        <img
+          className="w-8"
+          src="http://localhost:3000/separator.png"
+          draggable="true"
+          alt="breath"
+        ></img>
+        <img
+          className="w-8"
+          src="http://localhost:3000/breath.png"
+          draggable="true"
+          alt="breath"
+        ></img>
         <img
           className="w-8"
           src="http://localhost:3000/breath.png"
@@ -18,24 +36,6 @@ const IconList = () => {
           alt="breath"
         ></img>
       </div>
-      <img
-        className="w-8"
-        src="http://localhost:3000/separator.png"
-        draggable="true"
-        alt="breath"
-      ></img>
-      <img
-        className="w-8"
-        src="http://localhost:3000/breath.png"
-        draggable="true"
-        alt="breath"
-      ></img>
-      <img
-        className="w-8"
-        src="http://localhost:3000/breath.png"
-        draggable="true"
-        alt="breath"
-      ></img>
     </div>
   );
 };
